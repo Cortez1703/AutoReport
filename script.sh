89ce0989329725d7621e290dfeab3ee1dbe54959
cortez1703 <aleksahin6824@yandex.ru>
2024-10-15 00:11:43 +0300

    Change script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-cd /home/`whoami`/AutoReport &&/home/`whoami`/.local/share/virtualenvs/AutoReport-h54q4ACC/bin/python3 /home/`whoami`/AutoReport/script_upload_graphs.py
+
+username=$(whoami)
+
+echo $username
+
+namepipenv=$(ls /home/$username/.local/share/virtualenvs|grep AutoReport)
+
+echo $namepipenv
+
+
+cd /home/`whoami`/AutoReport &&/home/`whoami`/.local/share/virtualenvs/$namepipenv/bin/python3 /home/`whoami`/AutoReport/script_upload_graphs.py
