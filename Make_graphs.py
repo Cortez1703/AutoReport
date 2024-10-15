3acef05e4b68b2b344df0ee832912c3d7889c5ab
cortez1703 <aleksahin6824@yandex.ru>
2024-10-15 16:45:31 +0300

    change title2

--- Make_graphs.py
+++ Make_graphs.py
@@ -72,11 +72,9 @@ def make_graph(full_worktime: list, second_worktime: list,title:str):
     ax.plot(full_worktime, Y_full, "-o", label='Попыток захвата')
     ax.plot(second_worktime, Y_succesful_grab, "-o", label='Успешный захват')
     if len(full_worktime)<1:
-        ax.set_title(f'''{title}\nВсего захватов {len(full_worktime)}, 
-                     успешных захватов {len(second_worktime)}.КПД {(len(second_worktime) / (len(full_worktime)+1)) * 100:.2f}%''')
+        ax.set_title(f'''{title}\nВсего захватов {len(full_worktime)},успешных захватов {len(second_worktime)}.КПД {(len(second_worktime) / (len(full_worktime)+1)) * 100:.2f}%''')
     else:
-        ax.set_title(f'''{title}\nВсего захватов {len(full_worktime)}, 
-                     успешных захватов {len(second_worktime)}.КПД {(len(second_worktime) / (len(full_worktime))) * 100:.2f}%''')
+        ax.set_title(f'''{title}\nВсего захватов {len(full_worktime)},успешных захватов {len(second_worktime)}.КПД {(len(second_worktime) / (len(full_worktime))) * 100:.2f}%''')
     ax.title.set_fontsize(10)
     ax.legend()
     ax.grid(True)
