0c0549bd3b269b80741e249a8e3fc771adcb65ea
cortez1703 <aleksahin6824@yandex.ru>
2024-11-14 16:30:15 +0300

    123

--- script_upload_graphs.py
+++ script_upload_graphs.py
@@ -148,6 +148,7 @@ if __name__ == '__main__':
     print(1)
     count_of_data=Make_graphs.Save_PDF_images_grabs()
     Make_graphs.Save_PDF_images_grabs_gisto()
+    print(count_of_data)
     if count_of_data:
         try:
             print(1)
