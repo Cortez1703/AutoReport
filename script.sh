928aa9caa498afa12e6e52becd47e5b0420acd5c
cortez1703 <aleksahin6824@yandex.ru>
2024-11-14 14:34:23 +0300

    3

--- script.sh
+++ script.sh
@@ -4,9 +4,9 @@ if [ -n "$1" ]
 then
 if [ -n "$2" ]
 then
-cd ../AutoReport && python3 script_upload_graphs.py $1 $2
+python3 script_upload_graphs.py $1 $2
 else
-cd ../AutoReport && python3 script_upload_graphs.py $1 
+python3 script_upload_graphs.py $1 
 fi
-else cd ../AutoReport && python3 script_upload_graphs.py
+else python3 script_upload_graphs.py
 fi
