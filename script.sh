c680c293a9e7a0870cd0c9f36b3684a4bba19604
cortez1703 <aleksahin6824@yandex.ru>
2024-11-14 14:10:07 +0300

    fix

--- script.sh
+++ script.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-username=$(whoami)
-
-namepipenv=$(ls /home/$username/.local/share/virtualenvs|grep AutoReport)
-
-chat_id=872965519
-
 if [ -n "$1" ]
 then
 if [ -n "$2" ]
 then
-cd ../AutoReport && ../.local/share/virtualenvs/$namepipenv/bin/python3 ../AutoReport/script_upload_graphs.py $1 $2
+cd ../AutoReport && python3 script_upload_graphs.py $1 $2
 else
-cd ../AutoReport && ../.local/share/virtualenvs/$namepipenv/bin/python3 ../AutoReport/script_upload_graphs.py $1 
+cd ../AutoReport && python3 script_upload_graphs.py $1 
 fi
-else cd ../AutoReport && ../.local/share/virtualenvs/$namepipenv/bin/python3 ../AutoReport/script_upload_graphs.py
+else cd ../AutoReport && python3 script_upload_graphs.py
 fi
